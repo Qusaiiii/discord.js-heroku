cbecd49d3371c1db6072866c35eea21cfbc77ab4
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-09 06:20:08 -0800

    Update index.js

--- index.js
+++ index.js
@@ -90,12 +90,6 @@ client.on('ready', () => {
 	console.log('ready!');
 });
 
-client.on('message', msg => {
-	if (!msg.content.startsWith(tokens.prefix)) return;
-	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0])) commands[msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0]](msg);
-});
-
-
 client.login(process.env.BOT_TOKEN);
 client.login('Mzg4NzgwMzgwODI1NzE0Njkw.DQ2Bqw.o_z2NTiitDX1EK4OzNk_WUQk31U')
 var prefix = ('*')
